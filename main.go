a3539c21a5ed1dba2f5359f159484219357a5865
lhuskyrescue <lhuskyrescue@gmail.com>
2025-10-30 03:06:52 +0300

    add post handler, update get handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -15,12 +16,11 @@ type Calculation struct {
 	Expression string `json:"expression"`
 	Result     string `json:"result"`
 }
-
-type CalculationRequest struct { //структура, отвечающая за вычисления
-	Expression string `json:"expression"`
-}
 type requestBody struct {
 	Task string `json:"task"`
+}
+type CalculationRequest struct { //структура, отвечающая за вычисления
+	Expression string `json:"expression"`
 }
 
 var calculations = []Calculation{} //инициализированный слайс
@@ -39,8 +39,8 @@ func calculateExpression(expression string) (string, error) { //функция, 
 
 }
 func getCalculations(c echo.Context) error {
-	response := fmt.Sprintf("%v", task)
-	return c.String(http.StatusOK, response)
+
+	return c.JSON(http.StatusOK, calculations)
 }
 func postCalculations(c echo.Context) error {
 	var req CalculationRequest
@@ -59,11 +59,29 @@ func postCalculations(c echo.Context) error {
 	calculations = append(calculations, calc)
 	return c.JSON(http.StatusCreated, calc)
 }
+func postTask(c echo.Context) error {
+	var reqBody requestBody
+	decoder := json.NewDecoder(c.Request().Body)
+	if err := decoder.Decode(&reqBody); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid JSON"})
+	}
+	task = reqBody.Task
+	return c.JSON(http.StatusOK, map[string]string{
+		"message": "Task saved successfully",
+		"task":    task,
+	})
+}
+func getHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "hello "+task)
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
 	e.GET("/calculations", getCalculations)
+	e.GET("/", getHandler)
+	e.POST("/task", postTask)
 	e.POST("/calculations", postCalculations)
 	e.Start("localhost:8080")
 }
